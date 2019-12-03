ba90ebf718ce19a636d2a353e5c074f76b1a119a
LeonardoNasser <leonardonasser11@hotmail.com>
2019-12-03 11:44:59 -0300

    Java Api - Banco

---
+++ src/test/java/br/com/db1/db1start/bancoapi/BancoApiApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.db1.db1start.bancoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
